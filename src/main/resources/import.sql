bf053c042f5b380d291f9e8f1bd3f5266d9d1c57
XanderGI <xandergi.dev@gmail.com>
2026-03-17 21:50:23 +0500

    chore: configure multiline SQL extraction and add test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,30 @@
+INSERT INTO Players (name)
+VALUES ('Rafael Nadal'),
+       ('Roger Federer'),
+       ('Novak Djokovic'),
+       ('Andy Murray'),
+       ('Stan Wawrinka');
+
+INSERT INTO Matches (Player1, Player2, Winner)
+VALUES (1, 2, 1);
+
+INSERT INTO MAtches (Player1, Player2, Winner)
+VALUES (3, 2, 3),
+       (3, 2, 3),
+       (3, 2, 3),
+       (3, 2, 3),
+       (3, 2, 3),
+       (3, 2, 3);
+
+INSERT INTO Matches (Player1, Player2, Winner)
+VALUES (5, 2, 5),
+       (5, 2, 5),
+       (5, 2, 5),
+       (5, 2, 5),
+       (5, 2, 5);
+
+INSERT INTO Matches (Player1, Player2, Winner)
+VALUES (4, 2, 4),
+       (4, 2, 4),
+       (4, 2, 4),
+       (4, 2, 4);
