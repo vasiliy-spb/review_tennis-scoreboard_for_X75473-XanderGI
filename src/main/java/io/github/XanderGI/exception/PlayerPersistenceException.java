48ff60fc5194c74c41cdc57030c8b72ff9631312
XanderGI <xandergi.dev@gmail.com>
2026-04-04 13:40:39 +0500

    refactor: remove unused PlayerPersistenceException

--- src/main/java/io/github/XanderGI/exception/PlayerPersistenceException.java
+++
@@ -1,7 +0,0 @@
-package io.github.XanderGI.exception;
-
-public class PlayerPersistenceException extends RuntimeException {
-    public PlayerPersistenceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
