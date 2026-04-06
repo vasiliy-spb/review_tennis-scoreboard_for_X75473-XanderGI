ac1b3eabcac0aecc8865b2aae298c404c5ac58d1
XanderGI <xandergi.dev@gmail.com>
2026-04-06 16:31:44 +0500

    feat: add SearchUtil to tokenize and sanitize search queries

---
+++ src/main/java/io/github/XanderGI/util/SearchUtil.java
@@ -0,0 +1,22 @@
+package io.github.XanderGI.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SearchUtil {
+    public static List<String> tokenize(String filterName) {
+        if (filterName == null || filterName.isBlank()) {
+            return Collections.emptyList();
+        }
+
+        String clearFilterName = filterName.replace("-", " ").strip().replaceAll("\\s+"," ").toLowerCase();
+        String[] tokens = clearFilterName.split(" ");
+
+        return Arrays.asList(tokens);
+    }
+}
